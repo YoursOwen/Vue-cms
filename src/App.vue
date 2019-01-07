1f7180d6f0bd75dc4258ebb9ff8fe5c07083ba2f
YourOwen <minyuwen@126.com>
2019-01-07 13:27:35 +0800

    完成顶部

--- src/App.vue
+++ src/App.vue
@@ -13,19 +13,20 @@
 
     <!-- 底部nav -->
     <nav class="mui-bar mui-bar-tab">
-			<router-link class="mui-tab-item" to="/home">
+			<!-- mui-tab-item 类名有冲突，会导致没有办法切换，需要修改类名 -->
+			<router-link class="mui-tab-item1" to="/home">
 				<span class="mui-icon mui-icon-home"></span>
 				<span class="mui-tab-label">首页</span>
 			</router-link>
-			<router-link class="mui-tab-item" to="/member">
+			<router-link class="mui-tab-item1" to="/member">
 				<span class="mui-icon mui-icon-email"></span>
 				<span class="mui-tab-label">会员</span>
 			</router-link>
-			<router-link class="mui-tab-item" to="/shopcar">
+			<router-link class="mui-tab-item1" to="/shopcar">
 				<span class="mui-icon mui-icon-extra mui-icon-extra-cart"><span class="mui-badge">3</span></span>
 				<span class="mui-tab-label">购物车</span>
 			</router-link>
-			<router-link class="mui-tab-item" to="/search">
+			<router-link class="mui-tab-item1" to="/search">
 				<span class="mui-icon mui-icon-gear"></span>
 				<span class="mui-tab-label">搜索</span>
 			 </router-link>
@@ -61,6 +62,34 @@ export default {
 	transition: all .5s ease-in
 }
 
+
+.mui-bar-tab .mui-tab-item1.mui-active {
+  color: #007aff;
+}
+.mui-bar-tab .mui-tab-item1 {
+  display: table-cell;
+  overflow: hidden;
+  width: 1%;
+  height: 50px;
+  text-align: center;
+  vertical-align: middle;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  color: #929292;
+}
+.mui-bar-tab .mui-tab-item1 .mui-icon {
+    top: 3px;
+    width: 24px;
+    height: 24px;
+    padding-top: 0;
+    padding-bottom: 0;
+}
+.mui-bar-tab .mui-tab-item1 .mui-icon ~ .mui-tab-label {
+    font-size: 11px;
+    display: block;
+    overflow: hidden;
+    text-overflow: ellipsis;
+}
 
 
 </style>
